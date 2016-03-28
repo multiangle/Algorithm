f3095404e0e216464ef68be3acdf02149d1d4bd6
multiangle <461445957@qq.com>
2016-03-28 11:50:54 +0800

    二分查找 2016.3.28

--- src/multiangle/algorithm/ListInt.java
+++ src/multiangle/algorithm/ListInt.java
@@ -19,25 +19,38 @@ public class ListInt {
 
         int low = 0 ;
         int high = this.data.length-1 ;
-        while ( low <= high ) {
-            int target = ( low + high ) / 2 ;
-            if ( this.data[target] == element ){
-                return target ;
-            }
-            else if ( element < this.data[target] ) {
+        while ( low < high ) {
+            int target = ( low + high ) >> 1 ;
+            if ( element < this.data[target] ){
                 high = target - 1 ;
             }
-            else{
+            else if ( this.data[target] < element ) {
                 low = target + 1 ;
+            }
+            else{
+                return target ;
             }
         }
         return -1 ;
     }
 
+    private int binary_search_2(int element){
+
+        int low = 0 ;
+        int high = this.data.length  ;
+        while ( 1 < high-low){
+            int target = ( low + high ) >> 1 ;
+            if (element<this.data[target]) high = target ;
+            else low = target ;
+        }
+//        return element == this.data[low]?low:-1 ;
+        return low ;
+    }
+
     public static void main(String[] args){
-        int[] list = {1,2,3,4,5} ;
+        int[] list = {1,2,3,4,4,4,4,5,7} ;
         ListInt x = new ListInt(list) ;
-        int index = x.binary_search(6) ;
+        int index = x.binary_search_2(7) ;
         System.out.println(index);
     }
 }
