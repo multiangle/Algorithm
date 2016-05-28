5b0c9d4ae323e46d51abb613c2eb34b6ced0320a
multiangle <461445957@qq.com>
2016-05-28 10:13:20 +0800

    B树开个头

---
+++ src/multiangle/algorithm/BinaryTree/BTree.java
@@ -0,0 +1,10 @@
+package multiangle.algorithm.BinaryTree;
+
+/**
+ * Created by multiangle on 2016/5/28.
+ */
+public class BTree {
+    // 是一个多路平衡搜索树
+    // 所有叶节点深度统一相等，等效于外部节点深度统一
+    // 可以充分利用磁盘批量读写的优点
+}
