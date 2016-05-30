0e408945d93ab51976ba3638a3f0b30590f1ca66
multiangle <461445957@qq.com>
2016-05-30 11:00:35 +0800

    B树搜索和插入

--- src/multiangle/algorithm/BTree/BTNode.java
+++ src/multiangle/algorithm/BTree/BTNode.java
@@ -1,18 +1,51 @@
 package multiangle.algorithm.BTree;
 
+import java.util.Comparator;
 import java.util.Vector;
 
 /**
  * Created by multiangle on 2016/5/29.
  */
-public class BTNode<T> {
-    private BTNode<T> parent ;
-    private Vector<T> key ;
-    private Vector<BTNode> child ;
+public class BTNode<T extends Comparable<T>> {
+    protected BTNode<T> parent ;
+    protected Vector<T> key ;
+    protected Vector<BTNode> child ;
 
 
     public static void main(String[] args){
         Vector<Integer> a = new Vector() ;
         a.insertElementAt(1,0);
     }
+
+    public BTNode(){
+        parent = null ;
+        key = new Vector<T>() ;
+        child = new Vector<BTNode>() ;
+        child.add(0, null);
+    }
+
+    public BTNode(T value){
+        parent = null ;
+        key = new Vector<T>() ;
+        key.add(0,value);
+        child = new Vector<BTNode>() ;
+        child.add(0,null) ;
+        child.add(1,null) ;
+    }
+
+    /**
+     * 如果搜到值，则返回相应的值所在的数组位置。否则返回不大于目标的节点位置。
+     * 特别的，有可能返回-1值，表示目标小于数组中所有值。
+     * @param target
+     * @return
+     */
+    public int searchKey( T target ){
+        for (int i=0 ; i<key.size(); i++){
+            if (key.get(i).compareTo(target) == 0) return i ;
+            else if (key.get(i).compareTo(target) > 0) return i-1 ;
+        }
+        return key.size()-1 ;
+
+    }
+
 }
--- src/multiangle/algorithm/BTree/BTree.java
+++ src/multiangle/algorithm/BTree/BTree.java
@@ -3,15 +3,41 @@ package multiangle.algorithm.BTree;
 /**
  * Created by multiangle on 2016/5/29.
  */
-public class BTree<T> {
+public class BTree<T extends  Comparable<T>> {
     protected int _size,_order ;  // 关键码总数，阶次
     protected BTNode<T> _root,_hot ;
 
     protected void solveOverflow( BTNode<T> input ){}
     protected void solveUnderflow( BTNode<T> input ){}
 
-    public BTNode<T> search( T target ){ return null;}
-    public boolean insert( T input ){ return  true ;}
+    public BTree(){
+        _size = 0 ;
+        _root = new BTNode<T>() ;
+        _hot = null ;
+    }
+
+    public BTNode<T> search( T target ){
+        _hot = null ;
+        BTNode node = _root ;
+        if (_root == null) return null ;
+        while(node != null){
+            int index = node.searchKey(target) ;
+            if (index>=0 && node.key.get(index).equals(target)) return node ;
+            _hot = node ;
+            node = (BTNode)node.child.get(index+1) ;
+        }
+        return null ;
+    }
+    public boolean insert( T input ){
+        BTNode node = search(input) ;
+        if (node!=null) return false ;
+        int index = _hot.searchKey(input) ; //在_hot中确定插入位置
+        _hot.key.add(index+1, input);  // 将新关键码插至对应位置
+        _hot.child.add(index+2, null) ; // 创建一个空子树指针
+        _size++ ;
+        solveOverflow(_hot); // 如果发生上溢，则分裂。
+        return true ;
+    }
     public boolean remove( T output){ return true ;}
 
 }
