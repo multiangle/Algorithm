63ab25b3efeb3e2c9bcb02019123edf5d942d912
multiangle <461445957@qq.com>
2016-06-20 16:42:39 +0800

    PQ删除 下滤

--- src/multiangle/algorithm/PriorityQueue/PQ_ComplHeap.java
+++ src/multiangle/algorithm/PriorityQueue/PQ_ComplHeap.java
@@ -11,8 +11,47 @@ import java.util.Vector;
 public class PQ_ComplHeap<T> implements PQ_Interface<T>{ // 完全二叉堆
     protected ArrayList<PQ_Node<T>> data ;
 
-    protected void percolateDown(int n, int i){
+    PQ_ComplHeap(T[] A){
+        for (T a:A){
+            insert(a);
+        }
+    }
+    PQ_ComplHeap(){
+        data = new ArrayList<PQ_Node<T>>() ;
+    }
 
+    protected void swapNode(int a, int b){
+        PQ_Node temp = data.get(a) ;
+        data.set(a, data.get(b)) ;
+        data.set(b, temp) ;
+    }
+
+    protected void percolateDown(int index){
+        while (index*2+1 < data.size()){
+            if (index*2+2 < data.size()){ // 如果index有左右节点
+                PQ_Node<T> left = data.get(index*2+1) ;
+                PQ_Node<T> right = data.get(index*2+2) ;
+                if (data.get(index).priority < Math.max(left.priority,right.priority)){ //需要交换
+                    if (left.priority > right.priority) { // 与左节点交换
+                        swapNode(index, index*2+1);
+                        index = index*2+1 ;
+                    }else{ // 与右节点交换
+                        swapNode(index, index*2+2);
+                        index = index*2+2 ;
+                    }
+                }else{
+                    break ;
+                }
+            }else{ // 如果只有左节点
+                if (data.get(index).priority < data.get(index*2+1).priority){
+                    swapNode(index,index*2+1);
+                    break ;
+                }else{
+                    break ;
+                }
+
+            }
+        }
     }
 
     protected void percolateUp(int index){
@@ -34,31 +73,57 @@ public class PQ_ComplHeap<T> implements PQ_Interface<T>{ // 完全二叉堆
 
     }
 
-    PQ_ComplHeap(T[] A){
-        for (T a:A){
-            PQ_Node<T> n = new PQ_Node<T>(a) ;
-            insert(n);
-        }
-    }
-    PQ_ComplHeap(){
-        data = new ArrayList<PQ_Node<T>>() ;
+    public void insert(T value){
+        PQ_Node<T> n = new PQ_Node<T>(value) ;
+        data.add(n);
+        int i = data.size()-1 ;
+        percolateUp(i);
     }
 
-    public void insert(PQ_Node<T> node){ //按优先级次序插入词条
-        data.add(node);
+    public void insert(T value, int priority){ //按优先级次序插入词条
+        PQ_Node<T> n = new PQ_Node<T>(value, priority) ;
+        data.add(n);
         int i = data.size()-1 ;
-        percolateUp(i) ;
+        percolateUp(i);
     }
 
     public T getMax(){
-        return null ;
+        return data.get(0).value ;
     } //取出优先级最高词条
 
     public T delMax(){
-        return null ;
+        T max_element = data.get(0).value ;
+        data.set(0,data.remove(data.size()-1)) ;
+        percolateDown(0);
+        return max_element ;
     } //删除优先级最高词条
 
+    public String toString(){
+        String ret = "" ;
+        for (PQ_Node n:data){
+            ret += n.value ;
+        }
+        return ret ;
+    }
+
     public static void main(String[] args){
+        PQ_ComplHeap<Integer> pqc = new PQ_ComplHeap() ;
+        for (int i=0;i<10;i++){
+            pqc.insert(i,i);
+        }
+        System.out.println(pqc);
+        pqc.delMax() ;
+        System.out.println(pqc);
+        pqc.delMax() ;
+        System.out.println(pqc);
+        pqc.delMax() ;
+        System.out.println(pqc);
+        pqc.delMax() ;
+        System.out.println(pqc);
+        pqc.delMax() ;
+        System.out.println(pqc);
+        pqc.delMax() ;
+        System.out.println(pqc);
 
     }
 }
