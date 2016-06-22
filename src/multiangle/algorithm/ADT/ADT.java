5bcf9a02539e3f525d29b8ce16562ee0fd23afe6
multiangle <461445957@qq.com>
2016-06-23 00:28:53 +0800

    ADT

---
+++ src/multiangle/algorithm/ADT/ADT.java
@@ -0,0 +1,17 @@
+package multiangle.algorithm.ADT;
+
+/**
+ * Created by multiangle on 2016/6/23.
+ */
+public class ADT {
+    private char[] data ;
+
+    public int length(){}
+    public char charAt(int i){}
+    public ADT substr(int i, int k){}
+    public ADT prefix(int k){}
+    public ADT suffix(int k){}
+    public ADT concat(ADT T){}
+    public boolean equal(ADT T){}
+    public int indexOf(ADT P){}
+}
