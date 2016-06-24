ffa351f08d3a8d2bf01e1b4b69cff47569fd6a3f
multiangle <461445957@qq.com>
2016-06-24 11:31:47 +0800

    KMP

--- src/multiangle/algorithm/ADT/ADT.java
+++ src/multiangle/algorithm/ADT/ADT.java
@@ -4,14 +4,24 @@ package multiangle.algorithm.ADT;
  * Created by multiangle on 2016/6/23.
  */
 public class ADT {
-    private char[] data ;
+    public static int KMP(String sT, String sP){ // T,P分别表示文本和待搜索子串
+        char[] T = sT.toCharArray() ;
+        char[] P = sP.toCharArray() ;
+        int[] next = buildNext(P) ;
+        int n = T.length ; int m = P.length ;
+        int i=0, j = 0 ;
+        while (i<n && j<m){
+            if (0>j || T[i]==P[j]){
+                i++ ;
+                j++ ;
+            }else{
+                j = next[j] ;
+            }
+        }
+        return i-j ;
+    }
 
-    public int length(){}
-    public char charAt(int i){}
-    public ADT substr(int i, int k){}
-    public ADT prefix(int k){}
-    public ADT suffix(int k){}
-    public ADT concat(ADT T){}
-    public boolean equal(ADT T){}
-    public int indexOf(ADT P){}
+    private static int[] buildNext(char[] P){
+        return null ;
+}
 }
