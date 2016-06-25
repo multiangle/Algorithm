d3f5c5f982ef0d6e2f81d330fd19bd2ac94a4c3f
multiangle <461445957@qq.com>
2016-06-25 17:57:11 +0800

    KMP

--- src/multiangle/algorithm/ADT/ADT.java
+++ src/multiangle/algorithm/ADT/ADT.java
@@ -11,7 +11,7 @@ public class ADT {
         int n = T.length ; int m = P.length ;
         int i=0, j = 0 ;
         while (i<n && j<m){
-            if (0>j || T[i]==P[j]){
+            if (j<0 || T[i]==P[j]){
                 i++ ;
                 j++ ;
             }else{
@@ -21,7 +21,29 @@ public class ADT {
         return i-j ;
     }
 
-    private static int[] buildNext(char[] P){
-        return null ;
-}
+    private static int[] buildNext(char[] P){ // 在P[0,j)中，最大自匹配的真前缀和真后缀的长度
+        // next[j+1]应该 = 1+next[j] | 1+next[next[j]] 。。。中选择
+        int[] next = new int[P.length] ;
+        next[0] = -1 ;
+        for (int i=1; i<next.length; i++){
+            int index=next[i-1] ;
+            while (index > -1){
+                if (P[i-1] == P[index]){
+                    break ;
+                }else{
+                    index = next[index] ;
+                }
+            }
+            index += 1 ;
+            next[i] = index ;
+        }
+        return next ;
+    }
+
+    public static void main(String[] args){
+        int[] res = ADT.buildNext( "000100001".toCharArray() );
+        for (int r:res){
+            System.out.print(Integer.toString(r)+"\t");
+        }
+    }
 }
